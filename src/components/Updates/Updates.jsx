fa7344fbd4d9ad15ad01dc3e60253201985e6749
mdmahfuzrp <mdmahfuzrp@gmail.com>
2025-08-29 21:20:28 +0600

    user details

--- src/components/Updates/Updates.jsx
+++ src/components/Updates/Updates.jsx
@@ -1,24 +1,61 @@
 import React from "react";
-import "./Updates.css";
-import { UpdatesData } from "../../Data/Data";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../features/auth/authSlice";
+import { Card, CardContent, CardMedia, Typography, Box } from "@mui/material";
+import img1 from "../../imgs/img1.png"; // Default avatar if needed
+import img2 from "../../imgs/img2.png"; // Default avatar if needed
+import img3 from "../../imgs/img3.png"; // Default avatar if needed
 
 const Updates = () => {
+  const user = useSelector(selectUser);
+
+  if (!user) {
+    return <Typography variant="body1">No user is logged in.</Typography>;
+  }
+
+  // Select image based on role
+  const roleImages = {
+    admin: img1,
+    merchant: img2, // you can use different images per role
+    member: img3,
+  };
+
   return (
     <div className="Updates">
-      {UpdatesData.map((update) => {
-        return (
-          <div className="update">
-            <img src={update.img} alt="profile" />
-            <div className="noti">
-              <div  style={{marginBottom: '0.5rem'}}>
-                <span>{update.name}</span>
-                <span> {update.noti}</span>
-              </div>
-                <span>{update.time}</span>
-            </div>
-          </div>
-        );
-      })}
+      <Box sx={{ display: "flex", justifyContent: "center", mt: 4 }}>
+        <Card
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            p: 2,
+            boxShadow: 3,
+            width: 400,
+          }}
+        >
+          <CardMedia
+            component="img"
+            image={roleImages[user.role] || img1}
+            alt={user.role}
+            sx={{ width: 80, height: 80, borderRadius: "50%", mr: 2 }}
+          />
+          <CardContent sx={{ flex: "1 0 auto" }}>
+            <Typography variant="h6">
+              {user.name || user.storeName || user.email}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Role: {user.role}
+            </Typography>
+            {user.email && (
+              <Typography variant="body2" color="text.secondary">
+                Email: {user.email}
+              </Typography>
+            )}
+            <Typography variant="body2" color="text.secondary">
+              Last Login: {new Date(user.profile?.lastLogin).toLocaleString()}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Box>
     </div>
   );
 };
